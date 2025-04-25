df53b5503cd2301c9c9f24bd5d7464b8ddb7baa5
deepshikhavishwakarma <deepshikha2000deep@gmail.com>
2025-04-25 12:42:10 +0530

    improve test cases in junit

--- src/test/java/com/hcl/controller/PaymentControllerTest.java
+++ src/test/java/com/hcl/controller/PaymentControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,6 +29,7 @@ public class PaymentControllerTest {
 	    private ShoppingCartService shoppingCartService;
 
 	    @Test
+	    @WithAnonymousUser
 	    public void testShowPaymentPage_UnauthenticatedUser() throws Exception {
 	        // Act and Assert
 	        mockMvc.perform(get("/payment"))
